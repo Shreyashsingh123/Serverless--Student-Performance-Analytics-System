ebf4258f883282b6e438c46ffdd5655b42d5d60f
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-06-02 16:48:36 +0530

    Export the data into S3

--- scripts/utils/upload_to_s3.py
+++ scripts/utils/upload_to_s3.py
@@ -2,11 +2,11 @@ import boto3
 
 s3 = boto3.client('s3')
 
-file_path ="../../data/data.csv"   
-
+file_path ="../../data/student_data.json"   
+# file_path ="../../data/student_data.csv" 
 bucket_name = "student-performance-25"
-s3_key = "raw/student_performance_200.csv"
-
+s3_key = "raw/student_data.json"
+# s3_key = "raw/student_data.csv"
 s3.upload_file(
     file_path,
     bucket_name,
